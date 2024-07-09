e87438fd27230bfdcc87c27dfca7eb469ffe9af4
MohamedHegazy2020 <mohamedhegazy219427@gmail.com>
2024-07-09 15:47:31 +0300

    ....

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   envPrefix: "REACT_APP_",
-
-  plugins: [react() ],
+  server: { port: 8080 },
+  plugins: [react()],
   css: {
     preprocessorOptions: {
-      scss: {
-        
-      },
+      scss: {},
     },
   },
 });
